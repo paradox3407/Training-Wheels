231e030f4c642e3ef6ce615fab262b40e2e25921
paradox3407 <paradox3407@users.noreply.github.com>
2026-06-10 16:24:14 -0700

    Added MPU calibration program

---
+++ Firmware/Self-Balancing-Robot/src/calibration/MPU_calibration.cpp
@@ -0,0 +1,43 @@
+// include libraries
+#include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+MPU6050 mpu(Wire);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  byte mpuSucess = mpu.begin();
+  if (mpuSucess == 0){
+    Serial.println("MPU connected successfully!");
+  }
+  else {
+    Serial.println("MPU connecton failed.");
+    while (1) { // while true, so delay indefinitly 
+      delay(10); 
+    }
+  }
+  mpu.calcOffsets(true, true);
+  float accXoffset = mpu.getAccXoffset(); 
+  float accYoffset = mpu.getAccYoffset(); 
+  float accZoffset = mpu.getAccZoffset(); 
+  float gyroXoffset = mpu.getGyroXoffset();
+  float gyroYoffset = mpu.getGyroYoffset();
+  float gyroZoffset = mpu.getGyroZoffset();
+  Serial.print("accXoffset: ");
+  Serial.println(accXoffset, 10);
+  Serial.print("accYoffset: ");
+  Serial.println(accYoffset, 10);
+  Serial.print("accZoffset: ");
+  Serial.println(accZoffset, 10);
+  Serial.print("gyroXoffset: ");
+  Serial.println(gyroXoffset, 10);
+  Serial.print("gyroYoffset: ");
+  Serial.println(gyroXoffset, 10);
+  Serial.print("gyroZoffset: ");
+  Serial.println(gyroXoffset, 10);
+}
+void loop() {
+  
+}
